e971f1886a352d34c7ddf242a9da158f038555b2
Felix Kalchschmid <flupppi@posteo.net>
2024-05-31 21:05:19 +0200

    added table + table material

--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class NewBehaviourScript : MonoBehaviour
 {
+    MeshRenderer m_MeshRenderer;
+    private void Awake()
+    {
+        
+    }
     /* Start is called before the first frame update */
     void Start()
     {
-        
+        m_MeshRenderer = gameObject.GetComponent<MeshRenderer>();
+
     }
 
     /* Update is called once per frame */
     void Update()
     {
         
+        m_MeshRenderer.enabled = false;
     }
 }
