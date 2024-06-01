11870171a25ecc705691ba5253fe6958e3d85488
Felix Kalchschmid <flupppi@posteo.net>
2024-06-01 14:49:56 +0200

    added Card assets + spawning cards

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public enum CardState
 {
@@ -12,8 +14,24 @@ public enum CardState
 public class Card : MonoBehaviour
 {
     [SerializeField] private Color color;
+    [SerializeField] private TMP_Text titleTextField;
+    [SerializeField] private TMP_Text descriptionText;
+    [SerializeField] private Image icon;
+    [SerializeField] private TMP_Text revenueText;
+    [SerializeField] private TMP_Text minimumCostText;
+    [SerializeField] private TMP_Text influenceText;
     [SerializeField] private CardState cardState;
+    [SerializeField] private Player mostInfluencePlayer = null;
 
+
+
+
+    public string cardTitle;
+    public string cardDescription;
+    public Sprite cardIcon;
+    public int revenueIncrease;
+    public int minimumCost;
+    public int numberOfInfluence;
     void Start()
     {
         
@@ -22,5 +40,16 @@ public class Card : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void InitCard()
+    {
+        titleTextField.text = cardTitle;
+        descriptionText.text = cardDescription;
+        revenueText.text = revenueIncrease.ToString();
+        influenceText.text = numberOfInfluence.ToString();
+        minimumCostText.text = minimumCost.ToString();
+        icon.sprite = cardIcon;
+
     }
 }
--- Assets/Scripts/NewBehaviourScript.cs
+++ Assets/Scripts/NewBehaviourScript.cs
@@ -1,29 +1,49 @@
-using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngineInternal;
 
-public class NewBehaviourScript : MonoBehaviour
+public class Spawner : MonoBehaviour
 {
-    MeshRenderer m_MeshRenderer;
+    // The GameObject to instantiate.
+    public GameObject cardEntityToSpawn;
 
-    [SerializeField] private GameObject card;
-    private void Awake()
-    {
-        
-    }
-    /* Start is called before the first frame update */
+    // An instance of the ScriptableObject defined above.
+    
+
+    public List<SectorCardAsset> sectorsOfInfluence;
+    public List<SectorCardAsset> sectorsOfPower;
+    // This will be appended to the name of the created entities and increment when each is created.
+    int instanceNumber = 1;
+
     void Start()
     {
-        m_MeshRenderer = gameObject.GetComponent<MeshRenderer>();
-        Instantiate(card);
+        SpawnEntities(sectorsOfInfluence);  
+        SpawnEntities(sectorsOfPower);
     }
 
-    /* Update is called once per frame */
-    void Update()
+    void SpawnEntities(List<SectorCardAsset> sectors)
     {
-        
-        m_MeshRenderer.enabled = true;
-        
+        int currentSpawnPointIndex = 0;
+        foreach (SectorCardAsset spawnManagerValues in sectors) {
+            for (int i = 0; i < spawnManagerValues.numberOfPrefabsToCreate; i++)
+            {
+                // Creates an instance of the prefab at the current spawn point.
+                GameObject currentEntity = Instantiate(cardEntityToSpawn, spawnManagerValues.spawnPoints[currentSpawnPointIndex], Quaternion.identity);
+                // Sets the name of the instantiated entity to be the string defined in the ScriptableObject and then appends it with a unique number. 
+                currentEntity.name = spawnManagerValues.prefabName + instanceNumber;
+                Card card = currentEntity.GetComponent<Card>();
+                card.cardTitle = spawnManagerValues.cardTitle;
+                card.cardIcon = spawnManagerValues.cardIcon;
+                card.cardDescription = spawnManagerValues.cardDescription;
+                card.minimumCost = spawnManagerValues.minimumCost;
+                card.numberOfInfluence = spawnManagerValues.numberOfInfluence;
+                card.revenueIncrease = spawnManagerValues.revenueIncrease;
+                // Moves to the next spawn point index. If it goes out of range, it wraps back to the start.
+                currentSpawnPointIndex = (currentSpawnPointIndex + 1) % spawnManagerValues.spawnPoints.Length;
+
+                card.InitCard();
+                instanceNumber++;
+
+            }
+        }
     }
 }
